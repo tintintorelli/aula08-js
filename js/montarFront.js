518c8d97086ef983524f55ff3f2431c8dbd50a88
Daniel <git0123445@gmail.com>
2025-05-19 11:52:47 -0300

    08 - criando o arquivo montarFront.js usando o objeto shows - linkando com a área de proximos shows do menu

--- js/montarFront.js
+++ js/montarFront.js
@@ -1,2 +1,31 @@
-import { shows } from "./objetos.js";
-console.log(shows);
+import {
+    shows
+} from "./objetos.js";
+
+// Montando o grid de shows
+// passando o grid shows para o JS, será nele que os cards irão ficar
+const gridShows = document.querySelector('#gridShows');
+
+// montando os shows dentro dos cards
+shows.forEach(show => {
+    gridShows.innerHTML += `
+        <div class="col-md-6 col-lg-4 my-3">
+            <div class="card">
+                <a href="" class="text-decoration-none text-dark">
+                <img src="${show.banner}" class="card-img-top" alt="${show.artista}">
+                <div class="card-body">
+                        <h4 class="my-3">${show.artista}</h4>
+                        <div class="d-flex justify-content-between">
+                            <p>Local: ${show.local}</p>
+                            <p>Data: ${show.data}</p>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p>Horário: ${show.horario}</p>
+                            <p>Ingresso: ${show.tipo_ingresso}</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    `
+})
